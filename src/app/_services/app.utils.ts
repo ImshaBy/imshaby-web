82bb584373bbc377a7b8d7556d954d8b2174eb8f
Katsiaryna Laurynovich <katsiaryna_laurynovich@epam.com>
2018-09-14 10:36:44 +0200

    #116 ticket
    - added moment.js package
    - changed date format for desktop and mobile view

--- src/app/_services/app.utils.ts
+++ src/app/_services/app.utils.ts
@@ -2,32 +2,96 @@
 import {Injectable} from '@angular/core';
 
 import {Day} from '../_models/day';
+import * as moment from 'moment';
 
 @Injectable()
 export class Utils {
-  public DAYS = ['Нядзеля', 'Панядзелак', 'Аўторак', 'Серада', 'Чацвер', 'Пятнiца', 'Субота'];
-
   public getActualDays(): Day[] {
     let actualDays = [];
-    let now = new Date();
-    let counter = 0;
-    while (counter < 7) {
-      let date =  this.addDays(now, counter);
-      let day = new Day(date, this.DAYS[date.getDay()], date.toDateString());
+
+    for (let counter = 0; counter < 7; counter++) {
+      let dateObject =  this.incrementDate(new Date(), counter);
+      let momentObject = this.incrementMoment(counter);
+
+      let formattedDayName = this.getFormattedDayName(momentObject);
+      let formattedDate = this.getFormattedDate(momentObject);
+
+      let day = new Day(dateObject, formattedDayName, formattedDate);
       actualDays.push(day);
-      counter++;
     }
+
     return actualDays;
   }
 
-  private addDays(date, days) {
+  /**
+   * Increment Date
+   * @param {Date} date
+   * @param {Number} days
+   * @returns {Date}
+   */
+  private incrementDate(date, days) {
     let result = new Date(date);
     result.setDate(result.getDate() + days);
-  return result;
-}
 
-  public getSelectedDay( date: Date){
-    return date.getDay();
+    return result;
   }
 
+  /**
+   *
+   * @param days
+   * @returns {moment.Moment}
+   */
+  private incrementMoment(days) {
+    return moment().add(days, 'days');
+  }
+
+  /**
+   * Get formatted date
+   *
+   * @param {moment} date
+   * @returns {any}
+   */
+  private getFormattedDate(date) {
+    let formatted;
+
+    if (this.isMobileWidth()) {
+      formatted = date.locale('be').format('L').replace(/\.[0-9][0-9][0-9][0-9]/, '');
+
+    } else {
+      formatted = date.locale('be').format('D MMMM');
+    }
+
+    return formatted;
+  }
+
+  /**
+   * Get formatted name of day
+   *
+   * @param {moment} date
+   * @returns {any}
+   */
+  private getFormattedDayName(date) {
+    let dayName;
+
+    if (this.isMobileWidth()) {
+      dayName = date.locale('be').format('ddd')
+    } else {
+      dayName = date.locale('be').format('dddd');
+    }
+
+    return dayName;
+  }
+
+  /**
+   * Checks that is mobile width
+   *
+   * @return {Boolean}
+   */
+  private isMobileWidth() {
+    return window.innerWidth < 768;
+  }
+
+  public getSelectedDay(date: Date) {
+    return date.getDay();
+  }
 }
