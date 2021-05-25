24ea5832226ddbdea698735b281418310db8c531
katsiaryna_laurynovich <katsiaryna_laurynovich@epam.com>
2021-05-25 17:02:07 +0200

    migrate angular 11 -> 12

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -55,4 +55,4 @@ const appRoutes: Routes = [
   { path: '**', redirectTo: '' }
 ];
 
-export const routing = RouterModule.forRoot(appRoutes);
+export const routing = RouterModule.forRoot(appRoutes, { relativeLinkResolution: 'legacy' });
